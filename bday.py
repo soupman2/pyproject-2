0f11a859ff13359a4d7b94c2edad4e1ffced8a8a
soupman2 <jaysanau@gmail.com>
2025-09-29 19:29:38 +1000

    project 2

---
+++ bday.py
@@ -0,0 +1,80 @@
+#birthday 1
+print("(putting a date like june 31st will work but should be treated as july 1st. you do not have to fill in all birthdays)")
+day=int(input("enter day: "))
+month=int(input("enter month: "))
+
+if day > 31:
+    print("invalid date")
+
+elif month > 12:
+    print("invalid date")
+
+else:
+    print("birthday 1:")
+    print(day, month, sep="/")
+
+#birthday 2
+
+day2=int(input("enter day: "))
+month2=int(input("enter month: "))
+
+
+if day > 31:
+    print("invalid date")
+
+elif month > 12:
+    print("invalid date")
+
+else:
+    print("birthday 2:")
+    print(day2, month2, sep="/")
+
+#birthday 3
+
+day3=int(input("enter day: "))
+month3=int(input("enter month: "))
+
+if day3 > 31:
+    print("invalid date")
+
+elif month3 > 12:
+    print("invalid date")
+
+else:
+    print("birthday 3:")
+    print(day3, month3, sep="/")
+
+#birthday 4
+
+day4=int(input("enter day: "))
+month4=int(input("enter month: "))
+
+
+if day4 > 31:
+    print("invalid date")
+
+elif month4 > 12:
+    print("invalid date")
+
+else:
+    print("birthday 4:")
+    print(day4, month4, sep="/")
+
+    #birthday 5
+
+day5=int(input("enter day: "))
+month5=int(input("enter month: "))
+
+
+if day5 > 31:
+    print("invalid date")
+
+elif month5 > 12:
+    print("invalid date")
+
+else:
+    print("birthday 5:")
+    print(day5, month5, sep="/")
+    
+    print("all birthdays have been written down")
+
